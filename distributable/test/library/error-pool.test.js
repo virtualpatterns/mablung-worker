df7fb8ca0bb8d6aeed90283907052084da991cda
virtualpatterns <code.virtualpatterns@gmail.com>
2020-05-17 01:51:23 -0400

    post-test

--- distributable/test/library/error-pool.test.js
+++ distributable/test/library/error-pool.test.js
@@ -6,12 +6,28 @@ Test('new ErrorPool()', async test => {
 
   let pool = new ErrorPool({ 'numberOfProcess': 2 });
   let error = await new Promise(resolve => {
-    pool.once('error', (processInformation, error) => {
-      resolve(error);
+
+    let error = [];
+    let onError = null;
+
+    pool.on('error', onError = (processInformation, _error) => {
+
+      error.push(_error);
+
+      if (error.length === pool.numberOfProcess * 2) {
+
+        pool.off('error', onError);
+        onError = null;
+
+        resolve(error);
+
+      }
+
     });
+
   });
 
-  test.is(error.code, 'ENOENT');
+  test.is(error[0].code, 'ENOENT');
 
 });
 //# sourceMappingURL=error-pool.test.js.map
