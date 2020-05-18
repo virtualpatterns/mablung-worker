94528a6ede57854dab335b86e3841d545dea87da
virtualpatterns <code.virtualpatterns@gmail.com>
2020-05-17 21:01:34 -0400

    post-test

---
+++ distributable/test/library/exit-pool.test.js
@@ -0,0 +1,33 @@
+import Test from 'ava';
+
+import { ExitPool } from './exit-pool.js';
+
+Test('new ExitPool()', async test => {
+
+  let pool = new ExitPool({ 'numberOfProcess': 2 });
+  let code = await new Promise(resolve => {
+
+    let code = [];
+    let onExit = null;
+
+    pool.on('exit', onExit = (processInformation, _code) => {
+
+      code.push(_code);
+
+      if (code.length === pool.numberOfProcess) {
+
+        pool.off('exit', onExit);
+        onExit = null;
+
+        resolve(code);
+
+      }
+
+    });
+
+  });
+
+  test.is(code[0], 0);
+
+});
+//# sourceMappingURL=exit-pool.test.js.map
