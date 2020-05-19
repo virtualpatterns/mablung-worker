08239f6fd3cf0f8e64fe2e73e1a7b5420a61d526
virtualpatterns <code.virtualpatterns@gmail.com>
2020-05-19 13:05:42 -0400

    post-test

--- distributable/test/library/worker-pool.test.js
+++ distributable/test/library/worker-pool.test.js
@@ -192,6 +192,7 @@ Test.only('WorkerPool.module.throwUncaughtException()', async test => {
   try {
     await test.notThrowsAsync(pool.module.throwUncaughtException()); // the pool should recreate exited processes
     await new Promise(resolve => setTimeout(resolve, 3000));
+    await test.notThrowsAsync(pool.ping());
   } finally {
     await pool.end();
   }
