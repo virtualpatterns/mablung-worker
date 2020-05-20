891dbe45ab51c031609ac2d43afa0c83db1fc1d9
virtualpatterns <code.virtualpatterns@gmail.com>
2020-05-19 23:41:47 -0400

    post-test

--- distributable/test/library/exit-client.test.js
+++ distributable/test/library/exit-client.test.js
@@ -5,7 +5,6 @@ import { ExitClient } from './exit-client.js';
 import { WorkerClientExitedError } from '../../index.js';
 
 Test('new ExitClient()', async test => {
-  let worker = new ExitClient();
-  await test.notThrowsAsync(worker.whenRejected(WorkerClientExitedError));
+  await test.notThrowsAsync(new ExitClient().whenRejected(WorkerClientExitedError));
 });
 //# sourceMappingURL=exit-client.test.js.map
