4abe1e9cc1f6506789eb01bd61c14fbace135c0a
virtualpatterns <code.virtualpatterns@gmail.com>
2020-08-09 22:57:23 -0400

    whitespace, re-add --silent, export package, implement check-dependency

--- source/sandbox/cjs/imp.cjs
+++
@@ -1,5 +0,0 @@
-import { PascalCase } from '../../library/import/pascal-case.js'
-
-// const { pascalCase: PascalCase } = _PascalCase
-
-console.dir(PascalCase)
--- source/sandbox/cjs/worker0.cjs
+++
@@ -1,7 +0,0 @@
-const Process = process
-
-const KEY = `worker-${Process.pid}`
-
-export function getKey() {
-  return KEY
-}
