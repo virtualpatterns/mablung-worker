c0dcf18b1f89fe6e937c3cd681b07edcd4fdf440
virtualpatterns <code.virtualpatterns@gmail.com>
2020-10-05 22:04:07 -0400

    removing console messages

--- source/library/create-worker-server.js
+++ source/library/create-worker-server.js
@@ -19,21 +19,21 @@ const Package = JSON5.parse(FileSystem.readFileSync(Require.resolve('../../packa
 
   Command.parse(Process.argv)
 
-  console.log('-'.repeat(80))
-  console.log(`Process.version               = '${Process.version}'`)
-  console.log(`Package.version               = 'v${Package.version}'`)
-  console.log(`Process.argv[0]               = '${Process.argv[0]}'`)
-  console.log(`Process.execArgv              = ${Utilities.inspect(Process.execArgv)}`)
-  console.log(`Process.argv[1]               = '${Path.relative(Process.cwd(), Process.argv[1])}'`)
-  console.log(`Command.workerServerClassPath = '${Path.relative(Process.cwd(), Command.workerServerClassPath)}'`)
-  console.log(`Command.importPath            = '${Path.relative(Process.cwd(), Command.importPath)}'`)
+  // console.log('-'.repeat(80))
+  // console.log(`Process.version               = '${Process.version}'`)
+  // console.log(`Package.version               = 'v${Package.version}'`)
+  // console.log(`Process.argv[0]               = '${Process.argv[0]}'`)
+  // console.log(`Process.execArgv              = ${Utilities.inspect(Process.execArgv)}`)
+  // console.log(`Process.argv[1]               = '${Path.relative(Process.cwd(), Process.argv[1])}'`)
+  // console.log(`Command.workerServerClassPath = '${Path.relative(Process.cwd(), Command.workerServerClassPath)}'`)
+  // console.log(`Command.importPath            = '${Path.relative(Process.cwd(), Command.importPath)}'`)
 
   let workerServerClass = null
   workerServerClass = await import(URL.pathToFileURL(Command.workerServerClassPath))
   workerServerClass = workerServerClass.default ? workerServerClass.default : workerServerClass
 
-  console.log(`workerServerClass.name        = ${workerServerClass.name}`)
-  console.log('-'.repeat(80))
+  // console.log(`workerServerClass.name        = ${workerServerClass.name}`)
+  // console.log('-'.repeat(80))
 
   await (new workerServerClass()).import(Command.importPath)
 
